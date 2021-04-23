6747d3f0e702aae2c65ef86aaa8193b364bd4b63
Brian Crisp <bcrisp084@yahoo.com>
2021-04-22 21:02:29 -0400

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Search from "./components/memoSearch"
 import List from "./components/memoList"
 import image from "./images/MainPage.jpg";
+import SignIn from "./components/login"
 import {BrowserRouter as Router,Route,Switch,Redirect} from "react-router-dom";
 function App() {
   return (
     <div className="App"  style={{backgroundImage: 'url('+image+')'}}>
       <Router>         
     <div className="App" >
+    <Route exact={true} path="/" component={SignIn} />
         <Route exact={true} path="/list" component={List} /> 
-        <Route exact={true} path="/" component={Search} />
+        <Route exact={true} path="/search" component={Search} />
+
     </div>
     </Router>
     </div>
